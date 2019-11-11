823ab70b273ee16de68af06c5c90d26cf6a89de1
toutobi9575 <57618445+toutobi9575@users.noreply.github.com>
2019-11-11 18:23:19 +0900

    Add files via upload

---
+++ ObjRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjRotation : MonoBehaviour
+{
+
+    public SerialHandler _SerialHandler;　//SerialHandler.csの参照
+    Vector3 rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        objRotation(_SerialHandler.message_);
+    }
+
+    void objRotation(string message)
+    {
+        string[] a;
+
+        a = message.Split("="[0]);
+        if (a.Length != 2) return;
+        int v = int.Parse(a[1]);
+        switch (a[0])
+        {
+            case "num":
+                Debug.Log("0");
+                break;
+            case "none":
+                Debug.Log("2");
+                break;
+        }
+        Quaternion AddRot = Quaternion.identity;
+        AddRot.eulerAngles = new Vector3(-rotation.x, 0, -rotation.y);
+
+        transform.rotation = AddRot;
+    }
+}
