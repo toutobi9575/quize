b4eab30b43ab19176187d924e51ebe01ff653ad4
toutobi9575 <57618445+toutobi9575@users.noreply.github.com>
2019-11-18 15:38:36 +0900

    Add files via upload

---
+++ SerialHandler.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+public class SerialHandler : MonoBehaviour
+{
+    public delegate void SerialDataReceivedEventHandler(string message);
+    public event SerialDataReceivedEventHandler OnDataReceived;
+
+    public string portName = "COM5";
+    public int baudRate = 115200;
+
+    private SerialPort serialPort_;
+    private Thread thread_;
+    private bool isRunning_ = false;
+
+    public string message_;
+    private bool isNewMessageReceived_ = false;
+
+
+    void Awake()
+    {
+        Open();
+
+    }
+
+    void Update()
+    {
+    }
+
+    void OnDestroy()
+    {
+        Close();
+    }
+
+    private void Open()
+    {
+        serialPort_ = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+
+        serialPort_.Open();
+
+        isRunning_ = true;
+
+        thread_ = new Thread(Read);
+        thread_.Start();
+    }
+
+    private void Close()
+    {
+        isRunning_ = false;
+
+        if (thread_ != null && thread_.IsAlive)
+        {
+            thread_.Join();
+        }
+
+        if (serialPort_ != null && serialPort_.IsOpen)
+        {
+            serialPort_.Close();
+            serialPort_.Dispose();
+        }
+    }
+
+    private void Read()
+    {
+        while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
+        {
+            try
+            {
+                message_ = serialPort_.ReadLine();
+                isNewMessageReceived_ = true;
+                //Debug.LogError(message_);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    public void Write(string message)
+    {
+        try
+        {
+            serialPort_.Write(message);
+            Debug.LogError(message);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
